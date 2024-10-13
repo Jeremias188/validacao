09f1ce290ef8a12269ef754d368abf4f76b3d9c3
Jeremias188 <145406602+Jeremias188@users.noreply.github.com>
2024-10-13 10:43:58 -0300

    Add files via upload

---
+++ Js/script.js.js
@@ -0,0 +1,110 @@
+import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
+const camposDoFormulario = document.querySelectorAll('[required]')
+const formulario = document.querySelector('[data-formulario]');
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value,
+    }
+
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href = "./abrir-conta-form-2.html";
+})
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+    campo.addEventListener("invalid", evento => evento.preventDefault())
+})
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'tooShort',
+    'customError'
+]
+
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um email válido."
+    },
+    rg: {
+        valueMissing: "O campo de RG não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um RG válido.",
+        tooShort: "O campo de RG não tem caractéres suficientes."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caractéres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
+    }
+}
+
+function verificaCampo(campo) {
+    let mensagem = "";
+    campo.setCustomValidity('');
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
+    }
+    tiposDeErro.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+            console.log(mensagem);
+        }
+    })
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validadorDeInput = campo.checkValidity();
+
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem;
+    } else {
+        mensagemErro.textContent = "";
+    }
+}
+
+function validacampos() {
+    if (document.getElementById("nome").value==""){
+        document.getElementById("valida-nome").innerHTML="O campo de nome não pode estar vazio.";
+    } else {document.getElementById("valida-nome").innerHTML="";}
+    if (document.getElementById("email").value==""){
+        document.getElementById("valida-email").innerHTML="O campo de e-mail não pode estar vazio.";
+    } else {document.getElementById("valida-email").innerHTML="";}
+    if (document.getElementById("rg").value==""){
+        document.getElementById("valida-rg").innerHTML="O campo de RG não pode estar vazio.";
+    } else {document.getElementById("valida-rg").innerHTML="";}
+    if (document.getElementById("cpf").value==""){
+        document.getElementById("valida-cpf").innerHTML="O campo de CPF não pode estar vazio.";
+    } else {document.getElementById("valida-cpf").innerHTML="";}
+    if (document.getElementById("aniversario").value==""){
+        document.getElementById("valida-aniversario").innerHTML="O campo de data de nascimento não pode estar vazio.";
+    } else {document.getElementById("valida-aniversario").innerHTML="";}
+    if (document.getElementById("termos").value==""){
+        document.getElementById("valida-termo").innerHTML="Você deve aceitar nossos termos antes de continuar.";
+    } else {document.getElementById("valida-termo").innerHTML="";}
+}
